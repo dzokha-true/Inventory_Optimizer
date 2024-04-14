f5cc408c37eb26d3d4749abdf7d986d44a086e99
Dzhokha <67861550+Dzhokha@users.noreply.github.com>
2024-04-13 20:53:45 -0400

    looking into sales.py

--- src/database/Sales.py
+++ src/database/Sales.py
@@ -29,27 +29,27 @@ class Sales(Product):
         self.data_base = client['CompanyDetails']
         self.sales_DB = self.data_base['SalesDone']
         
-    def revenue_calculator(num, cost):
+    def revenue_calculator(self, num, cost):
         # range of dates
         # add a date
         document = self.performance_DB.find_one({'num': 1})
         new_revenue = document.get('revenue', 0) + num * cost
         self.performance_DB.update_one({'num': 1}, {'$set': {'revenue': new_revenue}})
         
-    def reset_revenue():
+    def reset_revenue(self):
         document = self.performance_DB.find_one({'num': 1})
         self.performance_DB.update_one({'num': 1}, {'$set': {'revenue': 0}})
         
-    def ABC_revenue():
+    def ABC_revenue(self):
         #range of dates
         # tied to date
         #num * cost per SKU per item
         
         
-    def SKU_class():
+    def SKU_class(self):
         ...
         
-    def sales_to_csv():
+    def sales_to_csv(self):
         with open('sales.csv', 'w', newline='') as csvfile:
             fieldnames = ['date', 'SKU', 'product_name', 'num', 'cost']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -65,7 +65,7 @@ class Sales(Product):
             writer.writerow({'date': date, 'SKU': SKU, 'product_name': product_name, 'num': num, 'cost': cost})
         csvfile.close()
 
-    def csv_to_sales():
+    def csv_to_sales(self):
         if self.status != "Admin":
             print("You dont have access to this feature")
         else:
@@ -75,13 +75,13 @@ class Sales(Product):
                 self.place_order_DB.insert_one({'date': row[0], 'SKU': row[1], 'product_name': row[2], 'num': row[3], 'cost': row[4]})
             filename.close()
         
-    def add_sale():
+    def add_sale(self):
         ...
         
-    def delete_sale:
+    def delete_sale(self):
         ...
 
-    def update_sale():
+    def update_sale(self):
         ...
         
 
