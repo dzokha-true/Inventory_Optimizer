aebd905b897f1f08e1c099aae406940258466d90
Pree Simphliphan <pree.jedi@gmail.com>
2024-04-18 13:38:13 +0700

    Add files via upload

--- src/source/public/functions.js
+++ src/source/public/functions.js
@@ -1,11 +1,54 @@
-function isAuthenticated(username,password){
-    const validUsername = "Username";
-    const validPassword = "Password";
+// function isAuthenticated(username,password){
+//     const validUsername = "Username";
+//     const validPassword = "Password";
 
-    if (username == validUsername && password == validPassword){
-    return true;
-    } else {
-        return false;
-    }
-}
+//     if (username == validUsername && password == validPassword){
+//     return true;
+//     } else {
+//         return false;
+//     }
+// }
 
+// function runLoginScript(scriptPath="src/database/LoginSystem.py", args) {
+//     const python = spawn("../Inventory_Optimizer/.venv/bin/python", [scriptPath, ...args]);
+
+//     python.stdout.on('data', (data) => {
+//         console.log(`stdout: ${data}`);
+//         const loginResponse = data.toString().trim();
+        
+//         return loginResponse;
+//     });
+
+//     python.stderr.on('data', (data) => {
+//         console.error(`stderr: ${data}`);
+//     });
+//     python.on('close', (code) => {
+//         console.log(`child process exited with code ${code}`);
+//     });
+// }
+
+const { ipcRenderer } = require('electron');
+//const { remote } = require('electron');
+
+document.getElementById('loginForm').addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent the default form submission
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    ipcRenderer.send('perform-login', { username, password });
+  });
+  
+ipcRenderer.on('login-success', (event, arg) => {
+     // Load the page on successful login
+     window.location.href = '../views/map.html';
+    //remote.app.emit('map-page');
+    //alert("valid username or password");
+    //  console.log("success");
+   });
+  
+ipcRenderer.on('login-failure', (event, arg) => {
+    const messageDiv = document.getElementById('message');
+    messageDiv.innerText = arg; // Display the error message
+    //alert("Invalid username or password");
+    // console.log("error");
+  });
