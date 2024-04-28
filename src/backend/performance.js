0b86b59b2de9936b1a1ebdd2fc125078574dbfb1
Pree Simphliphan <pree.jedi@gmail.com>
2024-04-29 06:09:43 +0700

    Add files via upload

--- src/backend/performance.js
+++ src/backend/performance.js
@@ -14,16 +14,40 @@ ipcRenderer.on('performance-success', (event, data) => {
 
 // check KPI column names
 function addHTMLKPI(data) {
-    var table = document.getElementById('kpi');
+    var rev = document.getElementById('revenue_dash');
+    var gro = document.getElementById('gross_dash');
+    var COGS = document.getElementById('cogs_dash');
+    var itr = document.getElementById('ITR_dash');
+    var profit = document.getElementById('gross_profit_dash');
+    var avg_int = document.getElementById('average_inventory_dash');
+    var exp_int = document.getElementById('expected_inventory_dash');
+    var act_int = document.getElementById('actual_inventory_dash');
+    var shrink = document.getElementById('shrinkage_dash');
+    var shrink_per = document.getElementById('shrinkage_percent_dash');
 
     const our_data = JSON.parse(data.dataset);
 
-    table.innerHTML = "";
-    
-    table.innerHTML += "<li class=\"table-row\">"
-     + "<div class=\"col col-1\" data-label=\"gross\">" + our_data.gross + "</div>"
-     + "<div class=\"col col-2\" data-label=\"cogs\">" + our_data.cogs + "</div>"
-     + "<div class=\"col col-3\" data-label=\"ITR\">" + our_data.ITR + "</div> " + "</li>";
+    rev.innerHTML = "";
+    gro.innerHTML = "";
+    COGS.innerHTML = "";
+    itr.innerHTML = "";
+    profit.innerHTML = "";
+    avg_int.innerHTML = "";
+    exp_int.innerHTML = "";
+    act_int.innerHTML = "";
+    shrink.innerHTML = "";
+    shrink_per.innerHTML = "";
+
+    rev.innerHTML += our_data.revenue;
+    gro.innerHTML += our_data.gross;
+    COGS.innerHTML += our_data.cogs;
+    itr.innerHTML += our_data.ITR;
+    profit.innerHTML += our_data.gross_profit;
+    avg_int.innerHTML += our_data.average_inventory;
+    exp_int.innerHTML += our_data.expected_inventory;
+    act_int.innerHTML += our_data.actual_inventory;
+    shrink.innerHTML += our_data.shrinkage;
+    shrink_per.innerHTML = our_data.shrinkage_percent;
 }
 
 function addHTMLPerformance(){
