6ac49a8393148e404f4c5e8911b7387accdce5ad
Vugar Amirov <vamirov@bu.edu>
2024-04-28 05:33:19 -0400

    Update Place_Order.py

--- src/database/Place_Order.py
+++ src/database/Place_Order.py
@@ -1,24 +1,10 @@
-import sys
-import os
-import bcrypt
-import random
-from urllib.parse import quote_plus
-from time import ctime
-import subprocess
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-from pymongo.errors import ConnectionFailure
-from datetime import date
-import re
 from Sales import Sales
 
 letters = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 capitals = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
 numbers = (1,2,3,4,5,6,7,8,9,0)
-
-# ==================================
-# NEEDS ADDING REMOVING AND UPDATING
-# ==================================
 
 class Place_Order(Sales):
     
@@ -28,25 +14,14 @@ class Place_Order(Sales):
         client = MongoClient(URI, server_api=ServerApi('1'))
         self.data_base = client['CompanyDetails']
         self.place_order_DB = self.data_base['OrdersPlaced']
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
 
-
-
+    def place_order(self, date, SKU, product_name, quantity, price):
+        self.place_order_DB.insert_one({
+            "date": date,
+            "SKU": SKU,
+            "product_name": product_name,
+            "quantity": quantity,
+            "price": price,
+            "isReceived": False
+        })
+        self.SKU_class()
