9c813a56f952e99fa204bb8c9083505988a383c8
Pree Simphliphan <pree.jedi@gmail.com>
2024-04-28 03:41:12 -0400

    Update notification.js

--- src/backend/notification.js
+++ src/backend/notification.js
@@ -14,6 +14,25 @@ document.getElementById('settingPopup').addEventListener('click', (event) => {
     localStorage.setItem('end', end); 
   });
 
+  // check id later
+document.getElementById('fifolifoPopup').addEventListener('click', (event) => {
+    event.preventDefault();
+    const fifo = document.getElementById('fifo');
+    const lifo = document.getElementById('lifo');
+    const message = "fifo_lifo";
+    if(lifo.checked == true && fifo.checked == true){
+        alert("choose one of two options");
+    }else if(lifo.checked == false && fifo.checked == false){
+        alert("choose one");
+    }else{
+        // discuss with groups
+        ipcRenderer.send('change fifo lifo', {start,end,message});
+        const status = lifo.checked ? "lifo": "fifo" ;
+        // or set date in local storage and send them along with other info for function we need
+        localStorage.setItem('fifo-lifo', status);
+    }
+  });
+
 function addHTMLNoti(data) {
     var notibar = document.getElementById('notiPopup');
 
