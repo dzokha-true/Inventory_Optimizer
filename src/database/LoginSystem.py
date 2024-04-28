6b1d747f28f7971089fff5f92e2efc3dd6a05ff7
Vugar Amirov <vamirov@bu.edu>
2024-04-28 05:32:37 -0400

    Update LoginSystem.py

--- src/database/LoginSystem.py
+++ src/database/LoginSystem.py
@@ -1,46 +1,17 @@
-import sys
-import os
 import bcrypt
-import random
-from urllib.parse import quote_plus
-from time import ctime
-import subprocess
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-from pymongo.errors import ConnectionFailure
-from datetime import date
-import re
-import HelperFunctions
 
 letters = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 capitals = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
 numbers = (1,2,3,4,5,6,7,8,9,0)
 
-# ===============================
-# DONE
-# ===============================
-
 class LoginSystem:
-    
     def __init__(self):
         URI = "mongodb+srv://" + "Admin" + ":" + "Admin" + "@businessinventorychecke.hnarzhd.mongodb.net/?retryWrites=true&w=majority&appName=BusinessInventoryChecker&tlsInsecure=true"
         client = MongoClient(URI, server_api=ServerApi('1'))
         self.data_base = client['LoginSystem']
         self.login_DB = self.data_base['AccessDetails']
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     def login(self, username, password):
         user_check = self.login_DB.find_one({'username': username})
@@ -58,7 +29,7 @@ class LoginSystem:
         else:
             print("Wrong Password")
             return False
-            
+
     def register(self, username, password, status):
         user_check = self.login_DB.find_one({'username': username})
         case = False
@@ -109,7 +80,7 @@ class LoginSystem:
             self.login_DB.insert_one({'username': username, 'password': hashed_password, 'status': status, 'fiscal_year': self.fiscal_year, 'lifo_fifo': self.lifo_fifo})
             print("Success")
             return True
-    
+
     def change_fiscal_year(self, date, status):
         if status != "Admin":
             print("You can not change the fiscal year!")
@@ -123,11 +94,3 @@ class LoginSystem:
             return False
         self.login_DB.update_many({}, {"$set": {"lifo_fifo": lifo_fifo}})
         return True
-
-
-
-
-
-
-
-
