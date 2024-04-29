edb4f9484ab79bc77456f91ca67faff2ef5a28a8
Vugar Amirov <vamirov@bu.edu>
2024-04-29 12:01:15 -0400

    Update Received_Order.py

--- src/database/Received_Order.py
+++ src/database/Received_Order.py
@@ -18,7 +18,7 @@ class Received_Order(Place_Order):
 
     def received(self, data):
         split_data = data.split(',')
-        quantity = int(data[3])
+        quantity = int(split_data[3])
         now = datetime.now()
         date = datetime.strptime(f'{now.year}-{now.month}-{now.day}', '%Y-%m-%d')
         new_date = (str(date.year))
@@ -34,23 +34,22 @@ class Received_Order(Place_Order):
             self.received_order_DB.insert_one({
                 "date": new_date,
                 "SKU": split_data[1],
-                "product_name": data[28],
-                "price": data[27]
+                "product_name": split_data[2],
+                "price": split_data[4]
             })
-        if self.product_DB.find_one({'SKU': data[1]}):
-            item = self.product_DB.find_one({'SKU': data[1]})
+        if self.product_DB.find_one({'SKU': split_data[1]}):
+            item = self.product_DB.find_one({'SKU': split_data[1]})
             quantity = int(item.get("quantity"))
-            quantity += int(data[3])
+            quantity += int(split_data[3])
             self.product_DB.update_one({'SKU': split_data[1]}, {"$set": {"quantity": str(quantity)}})
-
         else:
             self.product_DB.insert_one({
                 "SKU": split_data[1],
                 "product_name": split_data[2],
-                "quantity": data[3],
-                "price": data,
+                "quantity": split_data[3],
+                "price": split_data[4],
             })
-        self.place_order_DB.update_one({'date': split_data[0], 'SKU': split_data[1], "product_name": split_data[2], "quantity": data[3], "price": data[4]}, {'$set': {"isReceived": True}})
+        self.place_order_DB.update_one({'date': split_data[0], 'SKU': split_data[1], "product_name": split_data[2], "quantity": split_data[3], "price": split_data[4]}, {'$set': {"isReceived": True}})
         self.SKU_class()
         print("Success")
 
@@ -74,8 +73,8 @@ class Received_Order(Place_Order):
         cursor = self.place_order_DB.find({}, {'_id': 0, 'date': 1, 'SKU': 1, 'product_name': 1, 'quantity': 1, 'price': 1})
         for document in cursor:
             if document.get('date', 0) >= fiscal_year_start_str and document.get('date', 0) <= fiscal_year_end_str:
-                expected += document.get('price', 0) * document.get('quantity', 0)
-        print(expected)
+                expected += int(float(document.get('price', 0))) * int(float(document.get('quantity', 0)))
+        return (expected)
 
     def actual_inventory(self):
         admin_user = self.login_DB.find_one({'status': 'Admin'})
@@ -97,11 +96,91 @@ class Received_Order(Place_Order):
         cursor = self.received_order_DB.find({}, {'_id': 0, 'date': 1, 'SKU': 1, 'product_name': 1, 'price': 1})
         for document in cursor:
             if document.get('date', 0) >= fiscal_year_start_str and document.get('date', 0) <= fiscal_year_end_str:
-                expected += document.get('price', 0)
-        print(actual)
+                actual += int(float(document.get('price', 0)))
+        return (actual)
 
     def shrinkage(self):
-        print(self.expected_inventory() - self.actual_inventory())
+        admin_user = self.login_DB.find_one({'status': 'Admin'})
+        date_str = admin_user.get('fiscal_year')
+        now = datetime.now()
+        current_year = now.year
+        date = datetime.strptime(f'{current_year}-{date_str}', '%Y-%m-%d')
+        if date > now:
+            start = datetime(current_year - 1, date.month, date.day)
+            end = datetime(current_year, date.month, date.day)
+            fiscal_year_start_str = start.strftime('%Y-%m-%d')
+            fiscal_year_end_str = end.strftime('%Y-%m-%d')
+        else:
+            start = datetime(current_year, date.month, date.day)
+            end = datetime(current_year + 1, date.month, date.day)
+            fiscal_year_start_str = start.strftime('%Y-%m-%d')
+            fiscal_year_end_str = end.strftime('%Y-%m-%d')
+        actual = 0
+        cursor = self.received_order_DB.find({}, {'_id': 0, 'date': 1, 'SKU': 1, 'product_name': 1, 'price': 1})
+        for document in cursor:
+            if document.get('date', 0) >= fiscal_year_start_str and document.get('date', 0) <= fiscal_year_end_str:
+                actual += int(float(document.get('price', 0)))
+        admin_user = self.login_DB.find_one({'status': 'Admin'})
+        date_str = admin_user.get('fiscal_year')
+        now = datetime.now()
+        current_year = now.year
+        date = datetime.strptime(f'{current_year}-{date_str}', '%Y-%m-%d')
+        if date > now:
+            start = datetime(current_year - 1, date.month, date.day)
+            end = datetime(current_year, date.month, date.day)
+            fiscal_year_start_str = start.strftime('%Y-%m-%d')
+            fiscal_year_end_str = end.strftime('%Y-%m-%d')
+        else:
+            start = datetime(current_year, date.month, date.day)
+            end = datetime(current_year + 1, date.month, date.day)
+            fiscal_year_start_str = start.strftime('%Y-%m-%d')
+            fiscal_year_end_str = end.strftime('%Y-%m-%d')
+        expected = 0
+        cursor = self.place_order_DB.find({}, {'_id': 0, 'date': 1, 'SKU': 1, 'product_name': 1, 'quantity': 1, 'price': 1})
+        for document in cursor:
+            if document.get('date', 0) >= fiscal_year_start_str and document.get('date', 0) <= fiscal_year_end_str:
+                expected += int(float(document.get('price', 0))) * int(float(document.get('quantity', 0)))
+        return (expected - actual)
 
     def shrinkage_percent(self):
-        print((self.expected_inventory() - self.actual_inventory()) / self.expected_inventory()) * 100
+        admin_user = self.login_DB.find_one({'status': 'Admin'})
+        date_str = admin_user.get('fiscal_year')
+        now = datetime.now()
+        current_year = now.year
+        date = datetime.strptime(f'{current_year}-{date_str}', '%Y-%m-%d')
+        if date > now:
+            start = datetime(current_year - 1, date.month, date.day)
+            end = datetime(current_year, date.month, date.day)
+            fiscal_year_start_str = start.strftime('%Y-%m-%d')
+            fiscal_year_end_str = end.strftime('%Y-%m-%d')
+        else:
+            start = datetime(current_year, date.month, date.day)
+            end = datetime(current_year + 1, date.month, date.day)
+            fiscal_year_start_str = start.strftime('%Y-%m-%d')
+            fiscal_year_end_str = end.strftime('%Y-%m-%d')
+        actual = 0
+        cursor = self.received_order_DB.find({}, {'_id': 0, 'date': 1, 'SKU': 1, 'product_name': 1, 'price': 1})
+        for document in cursor:
+            if document.get('date', 0) >= fiscal_year_start_str and document.get('date', 0) <= fiscal_year_end_str:
+                actual += int(float(document.get('price', 0)))
+        admin_user = self.login_DB.find_one({'status': 'Admin'})
+        date_str = admin_user.get('fiscal_year')
+        now = datetime.now()
+        current_year = now.year
+        date = datetime.strptime(f'{current_year}-{date_str}', '%Y-%m-%d')
+        if date > now:
+            start = datetime(current_year - 1, date.month, date.day)
+            end = datetime(current_year, date.month, date.day)
+            fiscal_year_start_str = start.strftime('%Y-%m-%d')
+            fiscal_year_end_str = end.strftime('%Y-%m-%d')
+        else:
+            start = datetime(current_year, date.month, date.day)
+            end = datetime(current_year + 1, date.month, date.day)
+            fiscal_year_start_str = start.strftime('%Y-%m-%d')
+            fiscal_year_end_str = end.strftime('%Y-%m-%d')
+        expected = 0
+        cursor = self.place_order_DB.find({}, {'_id': 0, 'date': 1, 'SKU': 1, 'product_name': 1, 'quantity': 1, 'price': 1})
+        for document in cursor:
+            if document.get('date', 0) >= fiscal_year_start_str and document.get('date', 0) <= fiscal_year_end_str:
+                expected += int(float(document.get('price', 0))) * int(float(document.get('quantity', 0)))
+        return ((expected - actual) / expected) * 100
