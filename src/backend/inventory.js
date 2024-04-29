c926bdbc8adbdcb34e0b89e5c3e5c85b854e3b7a
Vugar Amirov <vamirov@bu.edu>
2024-04-29 04:13:55 -0400

    Update inventory.js

--- src/backend/inventory.js
+++ src/backend/inventory.js
@@ -8,53 +8,43 @@ const pageSize = 50;
 let loading = false;
 const container = document.getElementById('table-body');
 
-ipcRenderer.on('inventory_table_success', (event, data) => {
-	abc ++;
-	const our_data = JSON.parse(data.dataset);
-	if (loading) {
-		const tbody = document.getElementById('table-body');
-		our_data.forEach((item) => {
-			const tr = document.createElement('tr');
-			tr.innerHTML = `
-				<td>${item.date}</td>
-				<td>${item.SKU}</td>
-				<td>${item.product_name}</td>
-				<td>${item.cost}</td>
-				<td>${item.num || 'N/A'}</td>
-				`;
-			tbody.appendChild(tr);
-		});
-		page++;
-		loading = false;
-	}
-});
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const tableBody = document.getElementById('main_container');
-    console.log("DOMContentLoaded - the script is running");
+
+    ipcRenderer.on('product_table_success', (event, data) => {
+        abc ++;
+        const our_data = JSON.parse(data.dataset);
+        if (loading) {
+            const tbody = document.getElementById('table-body');
+            our_data.forEach((item) => {
+                const tr = document.createElement('tr');
+                tr.innerHTML = `
+                    <td>${item.SKU}</td>
+                    <td>${item.product_name}</td>
+                    <td>${item.quantity || 'N/A'}</td>
+                    <td>${item.price}</td>
+                    `;
+                tbody.appendChild(tr);
+            });
+            page++;
+            loading = false;
+        }
+    });
 
     tableBody.addEventListener('scroll', () => {
         const nearBottom = tableBody.scrollHeight - tableBody.scrollTop <= (tableBody.clientHeight + 20);
-        console.log(`Scrolled to: ${tableBody.scrollTop}, Near bottom: ${nearBottom}`);
 
         if (nearBottom && !loading) {
-            console.log('Approaching bottom: Loading more data...');
             loading = true;
-            ipcRenderer.send('create-inventory-table', { abc });
+            ipcRenderer.send('create-product-table', { abc });
         }
     });
 
     // Trigger initial data load
-    if (!loading) {
-        loading = true;
-        ipcRenderer.send('create-inventory-table', { abc });
-    }
+
 });
 
 if (!loading) {
     loading = true;
-    ipcRenderer.send('create-inventory-table', { abc });
+    ipcRenderer.send('create-product-table', { abc });
 }
-
-
