17db20fe7071cdad70ebae06cbcaac3b68373f85
VA <psyliney2016@gmail.com>
2024-05-02 04:47:41 -0400

    Commented LoginSystem.py

--- src/database/LoginSystem.py
+++ src/database/LoginSystem.py
@@ -1,21 +1,34 @@
+# Import modules needed
 import bcrypt
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-letters = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
-capitals = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-numbers = (1,2,3,4,5,6,7,8,9,0)
+# Initialize constants for input validation
+capitals = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+           'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+
 
 class LoginSystem:
+
+    # Initialize connection to the access details database
     def __init__(self):
-        URI = "mongodb+srv://" + "Admin" + ":" + "Admin" + "@businessinventorychecke.hnarzhd.mongodb.net/?retryWrites=true&w=majority&appName=BusinessInventoryChecker&tlsInsecure=true"
-        client = MongoClient(URI, server_api=ServerApi('1'))
+        self.username = None
+        self.status = None
+        self.fiscal_year = None
+        self.lifo_fifo = None
+        uri = ("mongodb+srv://Admin:Admin@businessinventorychecke.hnarzhd.mongodb.net/?retryWrites=true&w=majority"
+               "&appName=BusinessInventoryChecker&tlsInsecure=true")
+        client = MongoClient(uri, server_api=ServerApi('1'))
         self.data_base = client['LoginSystem']
         self.login_DB = self.data_base['AccessDetails']
 
+    # Function for logging in
     def login(self, username, password):
         user_check = self.login_DB.find_one({'username': username})
-        if user_check:    
+        if user_check:
             if bcrypt.checkpw(password.encode('utf8'), user_check['password']):
                 self.username = username
                 self.status = user_check.get('status')
@@ -23,13 +36,9 @@ class LoginSystem:
                 self.lifo_fifo = user_check.get('lifo_fifo')
                 print("Success")
                 return True
-            else:
-                print("Wrong Password")
-                return False
-        else:
-            print("Wrong Password")
-            return False
+        print("Wrong Password")
 
+    # Function for registering
     def register(self, username, password, status):
         user_check = self.login_DB.find_one({'username': username})
         case = False
@@ -39,24 +48,24 @@ class LoginSystem:
         capital = False
         special = False
         number = False
-        if capital == False or special == False or number == False or len(password) < 8:
+        if capital is False or special is False or number is False or len(password) < 8:
             if len(password) < 8:
-                print("Minimum 8 Length!")
+                print("Minimum 8 Characters!")
                 case = True
             for letter in password:
                 if letter in str(numbers):
-                        number = True
+                    number = True
                 if letter not in str(numbers) and letter not in letters and letter not in capitals:
                     special = True
                 if letter in capitals:
                     capital = True
-            if number == False:
+            if number is False:
                 print("Need Numbers!")
                 case = True
-            if special == False:
+            if special is False:
                 print("Need Special Characters!")
                 case = True
-            if capital == False:
+            if capital is False:
                 print("Need Capitals!")
                 case = True
         admin_user = self.login_DB.find_one({'status': 'Admin'})
@@ -65,7 +74,7 @@ class LoginSystem:
         elif status == "Admin":
             self.status = "Admin"
         elif status == "Read":
-            self.status ="Read"
+            self.status = "Read"
         else:
             print("Wrong Status!")
             case = True
@@ -77,10 +86,13 @@ class LoginSystem:
             self.fiscal_year = admin_user.get('fiscal_year')
             self.lifo_fifo = admin_user.get('lifo_fifo')
             hashed_password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-            self.login_DB.insert_one({'username': username, 'password': hashed_password, 'status': status, 'fiscal_year': self.fiscal_year, 'lifo_fifo': self.lifo_fifo})
+            self.login_DB.insert_one(
+                {'username': username, 'password': hashed_password, 'status': status, 'fiscal_year': self.fiscal_year,
+                 'lifo_fifo': self.lifo_fifo})
             print("Success")
             return True
 
+    # Function for only admin being able to change the fiscal year
     def change_fiscal_year(self, date, status):
         if status != "Admin":
             print("You can not change the fiscal year!")
@@ -88,6 +100,7 @@ class LoginSystem:
         self.login_DB.update_many({}, {"$set": {"fiscal_year": date}})
         return True
 
+    # Function for only the admin to change LIFO/FIFO computation
     def change_lifo_fifo(self, lifo_fifo, status):
         if status != "Admin":
             print("You can not change the life or fifo computation!")
