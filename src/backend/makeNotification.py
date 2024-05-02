5b341c51e653b998a6e2e63e0a9aec5ea1dae3c7
Vugar Amirov <vamirov@bu.edu>
2024-05-02 15:38:20 -0400

    Delete src/backend/makeNotification.py

--- src/backend/makeNotification.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-def makeNotification(SKU):
-    EOQ = np.random.choice([47,96,135,79, 88])
-    notification = f"Stock level of SKU {SKU} is below reorder point! Replenishment needed! Economic order quantity is {EOQ}."
-    print(notification) 
