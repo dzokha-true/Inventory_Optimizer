bb2874a196536ceecf257945943d57d0fd39e608
VA <psyliney2016@gmail.com>
2024-05-02 14:07:39 -0400

    Commented place order

--- src/database/Place_Order.py
+++ src/database/Place_Order.py
@@ -1,17 +1,13 @@
+# Import modules
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from Sales import Sales
 
-letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-'y', 'z')
-capitals = (
-'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z')
-numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
 
-
+# Place Order class which inherits from Sales class
 class Place_Order(Sales):
 
+    # Constructor which calls the parent class constructor and assigns a place_order_DB variable for a database access
     def __init__(self):
         super().__init__()
         URI = ("mongodb+srv://Admin:Admin@businessinventorychecke.hnarzhd.mongodb.net/?retryWrites=true&w=majority"
@@ -20,10 +16,11 @@ class Place_Order(Sales):
         self.data_base = client['CompanyDetails']
         self.place_order_DB = self.data_base['OrdersPlaced']
 
-    def place_order(self, date, SKU, product_name, quantity, price):
+    # Function called place order which updated the database to add a new order which is in transit
+    def place_order(self, date, sku, product_name, quantity, price):
         self.place_order_DB.insert_one({
             "date": date,
-            "SKU": SKU,
+            "SKU": sku,
             "product_name": product_name,
             "quantity": quantity,
             "price": price,
